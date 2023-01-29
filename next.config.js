37c307545f04d520d5eb7bcf5f5d1895d83235f9
zaid239 <zaidi.workspace@gmail.com>
2023-01-29 12:10:17 +0500

    complete code challange

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    images: { domains: ['i.imgflip.com'] },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+// i.imgflip.com
